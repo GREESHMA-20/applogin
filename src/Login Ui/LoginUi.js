483e4d854d7de3030daf51b33a2425da245f046f
GREESHMA-20 <Greeshma.G@deeandlee.com>
2022-07-04 12:53:09 +0530

    commit

--- src/Login Ui/LoginUi.js
+++ src/Login Ui/LoginUi.js
@@ -1,41 +1,32 @@
 import './LoginUi.css';
-import profile from "./../image/a.png";
-import email from "./../image/email.jpg";
-import pass from "./../image/pass.png";
+
 function LoginUi() {
   return (
     <div className="main">
      <div className="sub-main">
-       <div>
-         <div className="imgs">
-           <div className="container-image">
-             <img src={profile} alt="profile" className="profile"/>
+       
 
-           </div>
-
-
-         </div>
          <div>
            <h1>Login Page</h1>
            <div>
-             <img src={email} alt="email" className="email"/>
-             <input type="text" placeholder="user name" className="name"/>
+             
+             <input type="text" placeholder="user name / e-mail" className="name"/>
            </div>
            <div className="second-input">
-             <img src={pass} alt="pass" className="email"/>
-             <input type="password" placeholder="user name" className="name"/>
+            
+             <input type="password" placeholder="password" className="name"/>
            </div>
           <div className="login-button">
-          <button>Login</button>
+            <button><a href="#"> Login </a></button>
           </div>
            
             <p className="link">
-              <a href="#">Forgot password ?</a> Or<a href="#">Sign Up</a>
+              <a href="#">Forgot password ?</a> Or <a href="#">Sign Up</a>
             </p>
            
  
          </div>
-       </div>
+       
        
 
      </div>
